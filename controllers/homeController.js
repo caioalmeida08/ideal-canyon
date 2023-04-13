c7375e9951ef20d259043529b8a568bb89a8950b
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-04-13 11:37:36 -0300

    made changes in the navbar css and implemented hero section for mobile and desktop

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,6 @@
 const homeController = {
     index(req, res) {
-        res.render('homeView/index.ejs', { title: 'Ideal Canyon', styles: ['navbar_light'], navItems: [{ title: 'Modelos', url: '/' }, { title: 'Contatos', url: '/' }, { title: 'Sobre nós', url: '/' }] });
+        res.render('homeView/index.ejs', { title: 'Ideal Canyon', styles: ['navbar_light', 'home'], navItems: [{ title: 'Modelos', url: '/' }, { title: 'Contatos', url: '/' }, { title: 'Sobre nós', url: '/' }] });
     },
 };
 
