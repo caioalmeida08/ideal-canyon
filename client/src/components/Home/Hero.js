5471d55adb543eac0659d7c9e7568f88dce8311d
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-04-29 12:22:38 -0300

    created Buttons (ButtonPrimary and ButtonSecondary) component

--- client/src/components/Home/Hero.js
+++ client/src/components/Home/Hero.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { ButtonPrimary } from '../Utils/Buttons';
 
 const Hero = () => {
     return (
         <>
             <section className="hero grid-col-2 side-bleed">
-
+                <div className='gap-h-32'>
+                    <h1 className='text-h1 text-center text-desktop-left'>
+                        A scooter que se <span className='text-main'>encaixa na sua vida</span> está aqui!
+                    </h1>
+                    <p className='text-p text-center text-desktop-left'>
+                        Nossa coleção de modelos abrange todas as necessidades de uma pessoa moderna: praticidade, durabilidade e eficiência são o nosso foco.
+                    </p>
+                </div>
+                <div>
+                    <ButtonPrimary url='/produtos' text='Ver Produtos' />
+                </div>
             </section>
         </>
     );
