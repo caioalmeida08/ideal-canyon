7b03a6ff526cbd4e7b67b9638c1fd4dc6ba54890
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-04-30 10:25:50 -0300

    added 'loading' message to Navbar navitems

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,13 +3,10 @@ import React, { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar/Navbar.jsx";
 import PageTitle from "@/components/Utils/PageTitle";
 
-type _NavLink = {
-    title: string;
-    url: string;
-};
-
 export default function Home() {
-    const [navItems, setNavItems] = useState(Array<_NavLink>);
+    const [navItems, setNavItems] = useState([
+        { title: "Carregando...", url: "#" },
+    ]);
 
     useEffect(() => {
         setNavItems([
