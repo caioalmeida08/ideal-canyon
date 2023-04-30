387724393e0c752bcc7d913948d66f75e4efcb04
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-04-30 00:19:02 -0300

    removed fetching of data from the front-end

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,18 +1,6 @@
 const sequelize = require('../database/db').sequelize;
 
 const homeController = {
-    index(req, res) {
-        let data = {
-            title: 'Ideal Canyon',
-            navItems: [
-                { title: 'Modelos', url: '/' },
-                { title: 'Contatos', url: '/' },
-                { title: 'Sobre nós', url: '/sobre' }]
-        }
-
-        res.json(data);
-
-    },
 };
 
 module.exports = homeController;
