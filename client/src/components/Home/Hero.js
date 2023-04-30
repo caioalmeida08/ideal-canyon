b97b145f02332752d08f61285e59646043c3f5e6
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-04-29 22:13:49 -0300

    moved all images to assets/img

--- client/src/components/Home/Hero.js
+++ client/src/components/Home/Hero.js
@@ -7,15 +7,15 @@ const Hero = () => {
             <section className="hero grid-col-2 side-bleed">
                 <div className='gap-h-32'>
                     <h1 className='text-h1 text-center text-desktop-left'>
-                        A scooter que se <span className='text-main'>encaixa na sua vida</span> está aqui!
+                        Nós temos a <span className='text-main'>scooter perfeita</span> que você está procurando.
                     </h1>
                     <p className='text-p text-center text-desktop-left'>
                         Nossa coleção de modelos abrange todas as necessidades de uma pessoa moderna: praticidade, durabilidade e eficiência são o nosso foco.
                     </p>
+                    <ButtonPrimary url='/produtos' text='Encontre a sua scooter' />
                 </div>
                 <div>
-                    <ButtonPrimary url='/produtos' text='Ver Produtos' />
-                    <ButtonSecondary url='/produtos' text='Ver Produtos' />
+                    <img className='hero-img' src='' alt='Scooter do modelo Canyon Elite na cor branca' />
                 </div>
             </section>
         </>
