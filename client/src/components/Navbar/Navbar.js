7b11d11a0f6286f8b667ae5d1d9c47a9f34f239a
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-04-29 22:17:45 -0300

    moved all components to ./components

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../../scss/components/navbar.scss';
-import LogoAndText from '../../assets/img/LogoAndText';
-import HamburgerSVG from '../../assets/img/HamburgerSVG';
+import LogoAndText from '../Utils/LogoAndText';
+import HamburgerSVG from '../Utils/HamburgerSVG';
 import PageTitle from '../Utils/PageTitle';
 import NavLink from './NavLink';
 
