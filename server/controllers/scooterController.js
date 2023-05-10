9a1fbf82133b5f163751f33cbe2c128f4a7a2206
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-10 15:22:31 -0300

    images now returned in findDetails of scooter controller

--- server/controllers/scooterController.js
+++ server/controllers/scooterController.js
@@ -1,6 +1,9 @@
 const { sequelize } = require('../database/db');
 const scooterModel = require('../models/scooterModel');
 
+const fs = require('fs');
+const path = require('path');
+
 const scooterController = {
     async findDetails(req, res) {
         try {
@@ -31,21 +34,39 @@ const scooterController = {
             LIMIT 1`,
                 { type: sequelize.QueryTypes.SELECT });
 
+            // extracts the object from the array
+            response = response[0];
+
+            // gets every available color for this model os scooter
             let allColors = await sequelize.query(`SELECT DISTINCT scooter_color FROM Scooter WHERE scooter_model_short = '${req.query.modelShort}'`, { type: sequelize.QueryTypes.SELECT });
             console.log(allColors)
             allColors = Object.values(allColors).map(color => color.scooter_color);
-            response[0].allColors = allColors;
+            response.allColors = allColors;
 
+            // gets every available model-short of scooter
             let allModelsShort = await sequelize.query(`SELECT DISTINCT scooter_model_short FROM Scooter`, { type: sequelize.QueryTypes.SELECT });
             allModelsShort = Object.values(allModelsShort).map(model => model.scooter_model_short);
-            response[0].allModelsShort = allModelsShort;
+            response.allModelsShort = allModelsShort;
 
+            // gets every available model of scooter
             let allModels = [];
             for (let i = 0; i < allModelsShort.length; i++) {
                 let model = await sequelize.query(`SELECT DISTINCT scooter_model FROM Scooter WHERE scooter_model_short = '${allModelsShort[i]}'`, { type: sequelize.QueryTypes.SELECT });
                 allModels.push(model[0].scooter_model);
             }
-            response[0].allModels = allModels;
+            response.allModels = allModels;
+
+            // gets the images of the scooter
+            const publicPath = path.join(__dirname, '..', 'public/img/scooters/');
+            const files = fs.readdirSync(publicPath);
+            let imgs = [];
+            for (file of files) {
+                // check if file is one of the images of the scooter
+                if (file.includes('canyon-elite-scooter')) {
+                    imgs.push(file)
+                }
+            }
+            response.imgs = imgs;
 
             return res.status(200).json(response);
         } catch (error) {
@@ -56,7 +77,7 @@ const scooterController = {
     async findAll(req, res) {
         try {
             const response = await scooterModel.findAll();
-            return res.status(200).json(response);
+            return res.status(200).json(files);
         } catch (error) {
             return res.status(500).json(error.message);
         }
