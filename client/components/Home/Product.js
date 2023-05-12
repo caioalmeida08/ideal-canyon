7f2d944cfa651ab37e40b9b1e8fc724ddc791fa7
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-12 00:25:15 -0300

    onTouch and onClick events handled for product images section

--- client/components/Home/Product.js
+++ client/components/Home/Product.js
@@ -10,10 +10,13 @@ const Product = () => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    const [sliderIndex, setSliderIndex] = useState(1);
+    // controls the reset of the product slider when changing models
+    const [lastModel, setLastModel] = useState(null);
+    const [currentModel, setCurrentModel] = useState(null);
 
     let prev, current, next = null;
 
+    // fetch the data for this model of scooter
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(`/api/scooter/details?modelShort=${modelShort}`);
@@ -24,84 +27,151 @@ const Product = () => {
         fetchData();
     }, [modelShort]);
 
-    const handleSlider = (e) => {
-        setSliderIndex(sliderIndex + 1 > 4 ? 1 : sliderIndex + 1)
+    // get images for the side display in products section
+    const getSideImages = (side) => {
+        let imgs = [];
 
-        const mainImage = document.querySelector(`.${style.main_image}`);
-
-        if (sliderIndex == 1) {
-            mainImage.attributes.src.value = `api/img/scooters/${data.scooter_model_short}${sliderIndex}.png`;
+        if (side == "left") {
+            data.imgs.slice(0, 3).map((img, index) => {
+                imgs.push(
+                    <>
+                        <img
+                            key={index}
+                            src={`api/img/scooters/${img}`}
+                            aria-hidden="true"
+                            data-index={index}
+                            onClick={(e) => { handleSideImage(e) }}
+                        />
+                    </>
+                )
+            })
         } else {
-            mainImage.attributes.src.value = `api/img/scooters/${data.scooter_model_short}${sliderIndex}.jpg`;
+            data.imgs.slice(3, 6).map((img, index) => {
+                imgs.push(
+                    <>
+                        <img
+                            key={index}
+                            src={`api/img/scooters/${img}`}
+                            aria-hidden="true"
+                            onClick={(e) => { handleSideImage(e) }}
+                        />
+                    </>
+                )
+            })
         }
 
-        const active = document.querySelector("#image_slider [data-active='true']");
-        active.removeAttribute('data-active');
-
-        const next = document.querySelector(`#image_slider [data-index='${sliderIndex}']`);
-        next.setAttribute('data-active', 'true');
+        return imgs
 
     }
 
+    // populate the image slider of mobile version
+    const getImgSlider = () => {
+        if (!data.imgs) {
+            return
+        }
+
+        return (
+            data.imgs.map((img, index) => {
+                if (index == 0) {
+                    return (
+                        <div className={style.slider} data-active="true" data-index={index}></div>
+                    )
+                }
+
+                return (
+                    <div className={style.slider} data-index={index}></div>
+                )
+            })
+        )
+    }
+
+    const handleSideImage = (e) => {
+        const src = e.target.getAttribute("src")
+        const mainImageElement = document.querySelector(`.${style.main_image}`)
+        mainImageElement.setAttribute("src", src)
+
+    }
+
+    // handle the image slider of mobile version
+    const handleSlider = (e) => {
+        // get coordinates of the click
+        let x = e.changedTouches[0].clientX
+
+        // check if the click was on the right or the left side of the element
+        let width = e.target.offsetWidth
+
+        // get the index of the current image being displayed
+        try {
+            var currentIndex = document.querySelector(`#image_slider [data-active="true"]`).getAttribute("data-index");
+            currentIndex = parseInt(currentIndex)
+        } catch (error) {
+            var currentIndex = 0
+        }
+
+        // stores the index of the next image to be displayed
+        let nextIndex = null
+
+        // checks if the slider should go forwards or backwards
+        if (x >= width / 2) {
+            nextIndex = currentIndex + 1 > data.imgs.length - 1 ? 0 : currentIndex + 1
+        } else {
+            nextIndex = currentIndex - 1 < 0 ? data.imgs.length - 1 : currentIndex - 1
+        }
+
+        const mainImageElement = document.querySelector(`.${style.main_image}`)
+        mainImageElement.setAttribute("src", `api/img/scooters/${data.imgs[nextIndex]}`)
+
+        const sliderElement = document.getElementById("image_slider")
+
+        sliderElement.childNodes.forEach((element) => {
+            element.setAttribute("data-active", "false");
+        })
+
+        sliderElement.children[nextIndex].setAttribute("data-active", "true");
+    }
+
+    // resets the image slider to the first image when changing models
+    const resetSliderCounter = () => {
+        const nextIndex = 0;
+
+        const mainImageElement = document.querySelector(`.${style.main_image}`)
+        mainImageElement.setAttribute("src", `api/img/scooters/${data.imgs[nextIndex]}`)
+
+        const sliderElement = document.getElementById("image_slider")
+
+        sliderElement.childNodes.forEach((element) => {
+            element.setAttribute("data-active", "false");
+        })
+
+        sliderElement.children[nextIndex].setAttribute("data-active", "true");
+    }
 
     if (!isLoading) {
 
+        // controls the change of the model of scooter
         current = data.allModelsShort.indexOf(data.scooter_model_short)
         prev = current - 1 < 0 ? data.allModelsShort.length - 1 : current - 1
         next = current + 1 > data.allModelsShort.length - 1 ? 0 : current + 1
-
-        let leftImages = [];
-        data.imgs.slice(1, 4).map((img, index) => {
-            leftImages.push(
-                <>
-                    <img
-                        key={index}
-                        src={`api/img/scooters/${img}`}
-                        aria-hidden="true"
-                        data-index={index + 2}
-                    />
-                </>
-            )
-        })
-
-        let right_images = [];
-        data.imgs.slice(4, 7).map((img, index) => {
-            right_images.push(
-                <>
-                    <img
-                        key={index}
-                        src={`api/img/scooters/${img}`}
-                        aria-hidden="true"
-                    />
-                </>
-            )
-        })
-
 
         return (
             <>
                 <section
                     className={`${style.products} side-bleed section-margin-top max-width-tablet-500 max-width-desktop-unset`}
                 >
-                    <div className={`${style.product_images} max-width-500 max-width-desktop-unset`} onClick={(e) => { handleSlider(e) }}>
+                    <div className={`${style.product_images} max-width-500 max-width-desktop-unset`} id="product_images" data-model={data.scooter_model_short} onTouchEnd={(e) => { handleSlider(e) }}>
                         <img
                             src={`api/img/scooters/${data.scooter_model_short}1.png`}
-                            alt="Imagem da Canyon Elite Scooter em fundo transparente"
+                            alt={`Imagem da ${data.scooter_model_short} em fundo transparente`}
                             className={`${style.main_image}`}
                         />
                         <div className={style.left_images}>
-                            {leftImages}
+                            {getSideImages("left")}
                         </div>
                         <div className={style.right_images}>
-                            {right_images}
+                            {getSideImages("right")}
                         </div>
-                        <div className={style.image_slider} id="image_slider"
-                            onClick={(e) => { handleSlider(e) }}
-                        >
-                            <div className={style.slider} data-index="4"></div>
-                            <div className={style.slider} data-index="1" data-active="true"></div>
-                            <div className={style.slider} data-index="2"></div>
-                            <div className={style.slider} data-index="3"></div>
+                        <div className={style.image_slider} id="image_slider">
+                            {getImgSlider()}
                         </div>
                     </div>
                     <div className={`${style.product_informations} max-width-500 max-width-desktop-unset`}>
@@ -148,7 +218,7 @@ const Product = () => {
                         </div>
                     </div>
 
-                    <div className={style.other_product} onClick={() => setModelShort(data.allModelsShort[prev])}>
+                    <div className={style.other_product} onClick={() => { setModelShort(data.allModelsShort[prev]); resetSliderCounter() }}>
                         <img src={`api/img/scooters/${data.allModelsShort[prev]}1.png`} aria-hidden="true" />
                         <h2 className="text-capitalize">{data.allModels[prev]}</h2>
                         <button
@@ -156,7 +226,7 @@ const Product = () => {
                             aria-label="Ver informações da Canyon Comfort Scooter"
                         ></button>
                     </div>
-                    <div className={style.other_product} onClick={() => setModelShort(data.allModelsShort[next])}>
+                    <div className={style.other_product} onClick={() => { setModelShort(data.allModelsShort[next]); resetSliderCounter() }}>
                         <img src={`api/img/scooters/${data.allModelsShort[next]}1.png`} aria-hidden="true" />
                         <h2 className="text-capitalize">{data.allModels[next]}</h2>
                         <button
