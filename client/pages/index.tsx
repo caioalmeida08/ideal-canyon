14a35f8bbd82d6a91d51d181ea0850ded9cce573
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-13 12:34:34 -0300

    made the benefits section its own component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,14 +6,11 @@ import PageTitle from "@/components/Utils/PageTitle";
 import { ButtonPrimary } from "@/components/Utils/Buttons";
 
 import Review from "@/components/Home/Review";
+import Benefits from "@/components/Home/Benefits";
 import Product from "@/components/Home/Product";
 import DoubtsForm from "@/components/Home/DoubtsForm";
 import Faq from "@/components/Home/Faq";
 import Footer from "@/components/Footer/Footer";
-
-import VelocimeterIcon from "@/assets/img/icons/icon_velocimeter.png";
-import CheapIcon from "@/assets/img/icons/icon_cheap.png";
-import ShieldIcon from "@/assets/img/icons/icon_shield.png";
 
 import style from "@/scss/pages/index.module.scss";
 
@@ -36,58 +33,7 @@ export default function Home() {
             <Navbar {...navItems} />
             <Hero />
             <Review />
-            <section
-                className={`${style.benefits} section-dark section-margin-top`}
-            >
-                <h2 className="text-main text-center text-h2">
-                    Só nas nossas scooters você encontra
-                </h2>
-                <div>
-                    <div
-                        className={`${style.card} text-center text-light max-width-500`}
-                    >
-                        <img
-                            className={style.card_icon}
-                            src={VelocimeterIcon.src}
-                            aria-hidden="true"
-                        />
-                        <h3 className="text-bold">Potência excepcional</h3>
-                        <p className="line-height-15">
-                            Nós projetamos nossas scooters com motores potentes,
-                            que proporcionam uma condução suave e rápida.
-                        </p>
-                    </div>
-                    <div
-                        className={`${style.card} text-center text-light max-width-500`}
-                    >
-                        <img
-                            className={style.card_icon}
-                            src={CheapIcon.src}
-                            aria-hidden="true"
-                        />
-                        <h3 className="text-bold">Preço acessível</h3>
-                        <p className="line-height-15">
-                            Trabalhamos para manter nossos preços competitivos
-                            sem comprometer a qualidade do produto.
-                        </p>
-                    </div>
-                    <div
-                        className={`${style.card} text-center text-light max-width-500`}
-                    >
-                        <img
-                            className={style.card_icon}
-                            src={ShieldIcon.src}
-                            aria-hidden="true"
-                        />
-                        <h3 className="text-bold">Durabilidade superior</h3>
-                        <p className="line-height-15">
-                            Nossas scooters são construídas com materiais de
-                            alta qualidade e são resistentes ao desgaste diário.
-                        </p>
-                    </div>
-                </div>
-            </section>
-
+            <Benefits />
             <Product />
             <section
                 className={`${style.meet} section-dark section-margin-top grid-col-8-4`}
