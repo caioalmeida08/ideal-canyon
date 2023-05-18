461f72e5ca4054a3b4805c95b7fc17c30097b88e
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-18 01:09:48 -0300

    sucessfuly mapping network addresses and displaying them on server startup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,27 @@
+// Express imports
 import express from 'express'
-
 import { Router, Request, Response } from 'express';
 
-const app = express();
+// Network data
+import ipAddresses from './lib/networkinterfaces';
 
+// Server port
+const PORT = 5000;
+
+// Express app
+const app = express();
 const route = Router()
 
 app.use(express.json())
-
 route.get('/', (req: Request, res: Response) => {
   res.json({ message: 'hello world with Typescript' })
 })
 
 app.use(route)
-
-app.listen(3333, () => 'server running on port 3333')
+app.listen(PORT, () => {
+    console.clear();
+    ipAddresses.forEach((ip) => {
+        console.log(`Servidor rodando em: http://${ip}:${PORT}`);
+    }
+    )
+})
---
+++ server/src/lib/networkinterfaces.ts
@@ -0,0 +1,13 @@
+import Os  from 'os';
+
+// Reading network interfaces and showing them
+const ifaces = Os.networkInterfaces();
+const ipAddresses = Object.values(ifaces).map((iface) =>{
+    // iface is an array of objects/interfaces
+    // index 0 of each element is its mac address
+    // index 1 of each element is its ip address
+    
+    return iface?.[1]?.address;
+})
+
+export default ipAddresses;
