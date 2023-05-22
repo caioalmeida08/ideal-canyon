6fc5b6756c53a80c898f1997f3865630e5fab5e0
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-22 15:23:38 -0300

    implemented validateRequestBody to handle raw body inputs prior to database insertion in controllers

---
+++ server/src/lib/validateRequestBody.ts
@@ -0,0 +1,39 @@
+import { Request } from "express";
+import CustomValidationError from "./customValidationError";
+
+/**
+ * Validates if the request body is valid
+ * 
+ * Deletes the fields that should not be manually created/updated
+ * (model primary key, createdAt and updatedAt)
+ * 
+ * @param model Model to be validated (Class instance)
+ * @param req Request object
+ */
+const validateRequestBody = (Model: any, req: Request) => {
+    try {
+        // get the model primary key
+        const primaryKey = Model.primaryKeyAttribute;
+
+        // remove the fields that should not be manually created
+        delete req.body[primaryKey];
+        delete req.body.createdAt;
+        delete req.body.updatedAt;
+    
+        // check if every parameter sent exists
+        const modelAttributeKeys = Array.from(Object.keys(Model.getAttributes()));
+        const bodyAttributeKeys = Array.from(Object.keys(req.body));
+        
+        bodyAttributeKeys.forEach((attribute: string) => {
+            // if attribute not in requiredAttributeKeys, throw an error
+            if (!modelAttributeKeys.includes(attribute)) throw new CustomValidationError(`O atributo ${attribute} não existe.`, req.body[attribute]);
+        });
+    
+        // check if body is empty
+        if (Object.keys(req.body).length === 0) throw new CustomValidationError("O corpo da requisição não pode estar vazio.");         
+    } catch (error) {
+        throw error;
+    }
+}
+
+export default validateRequestBody;
