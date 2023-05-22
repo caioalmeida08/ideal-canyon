d6a13824334fe03303fff5332bade5f201d8fcee
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-21 22:15:54 -0300

    created contactModel

---
+++ server/src/models/contactModel.ts
@@ -0,0 +1,112 @@
+import { Model, InferAttributes, InferCreationAttributes, DataTypes, UUIDV4 } from "sequelize";
+
+import sequelize from "../database/db";
+
+class Contact extends Model<InferAttributes<Contact>, InferCreationAttributes<Contact>> {
+    declare contact_id: string;
+    declare contact_name: string;
+    declare contact_subject: string;
+    declare contact_message: string;
+    declare contact_agrees_with_terms: string;
+}
+
+Contact.init({
+    contact_id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+        validate: {
+            notEmpty: {
+                msg: "O id do contato não pode ser vazio.",
+            },
+            notNull: {
+                msg: "O id do contato não pode ser nulo.",
+            },
+            isUUID: {
+                args: 4,
+                msg: "O id do contato deve ser um UUID válido.",
+            },
+        },
+    },
+    contact_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "O nome de contato não pode ser vazio.",
+            },
+            notNull: {
+                msg: "O nome de contato não pode ser nulo.",
+            },
+            len: {
+                args: [3, 255],
+                msg: "O nome de contato deve ter entre 3 e 255 caracteres.",
+            },
+            is: {
+                args: /^[a-zA-ZÀ-ÿ\s]*$/,
+                msg: "O nome de contato deve conter apenas letras e espaços.",
+            },
+        },
+    },
+    contact_message: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "A mensagem não pode ser vazia.",
+            },
+            notNull: {
+                msg: "A mensagem não pode ser nula.",
+            },
+            len: {
+                args: [3, 1000],
+                msg: "A mensagem deve ter entre 3 e 1000 caracteres.",
+            },
+            isAlphanumeric: {
+                msg: "A mensagem deve conter apenas letras e números.",
+            },
+        },
+    },
+    contact_subject: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "O assunto não pode ser vazio.",
+            },
+            notNull: {
+                msg: "O assunto não pode ser nulo.",
+            },
+            len: {
+                args: [3, 255],
+                msg: "O assunto deve ter entre 3 e 255 caracteres.",
+            },
+            isAlphanumeric: {
+                msg: "O assunto deve conter apenas letras e números.",
+            },
+        },
+    },
+    contact_agrees_with_terms: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "O assunto não pode ser vazio.",
+            },
+            notNull: {
+                msg: "O assunto não pode ser nulo.",
+            },
+            isIn: {
+                args: [["on"]],
+                msg: "Você deve confirmar que concorda com os termos de uso.",
+            },
+        },
+    },
+}, { sequelize });
+
+(async () => {
+    await sequelize.sync();
+})();
+
+export default Contact;
