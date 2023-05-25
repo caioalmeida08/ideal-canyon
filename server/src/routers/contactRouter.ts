ab2c081d0ce83d6900a73ab85564b7745a317638
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-25 16:27:46 -0300

    changed misspelling (/contacts/ -> /contact/)

--- server/src/routers/contactRouter.ts
+++ server/src/routers/contactRouter.ts
@@ -4,35 +4,35 @@ import contactController from "../controllers/contactController";
 const contactRouter = Router();
 
 /**
- * GET /contacts/
+ * GET /contact/
  * returns all contacts
  * requires admin privileges
  */
 contactRouter.get("/", contactController.findAll);
 
 /**
- * GET /contacts/:contact_id
+ * GET /contact/:contact_id
  * returns a contact by id
  * requires admin privileges
  */
 contactRouter.get("/:contact_id", contactController.findOne);   
 
 /**
- * POST /contacts/
+ * POST /contact/
  * creates a new contact
  * requires admin privileges
  */
 contactRouter.post("/", contactController.create);
 
 /**
- * PUT /contacts/:contact_id
+ * PUT /contact/:contact_id
  * updates a contact by id
  * requires admin privileges
  */
 contactRouter.put("/:contact_id", contactController.update);
 
 /**
- * DELETE /contacts/:contact_id
+ * DELETE /contact/:contact_id
  * deletes a contact by id
  * requires admin privileges
  */
