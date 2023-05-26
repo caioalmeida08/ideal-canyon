877d283d185bc6595f52775e8082c10472fffa82
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-26 10:52:34 -0300

    fixed path to database

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './server/src/database/idealcanyon.sqlite',
+    storage: './src/database/idealcanyon.sqlite',
     typeValidation: true,
     logging: false
 });
