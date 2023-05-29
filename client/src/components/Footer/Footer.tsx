e4902be4569ca73eadc5801126cd45ba7d7f276f
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-29 13:58:16 -0300

    fix: converted to .tsx

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,65 @@
+import style from "./Footer.module.scss";
+import LogoAndText from "../Utils/LogoAndText";
+import { FunctionComponent } from "react";
+
+interface FooterProps {}
+
+const Footer: FunctionComponent<FooterProps> = () => {
+  return (
+    <>
+      <footer
+        className={`${style.footer} section-dark section-margin-top`}
+        id="footer"
+      >
+        <LogoAndText />
+        <div>
+          <div className="footer-informations">
+            <h3 className="text-main text-center text-demi">Informações</h3>
+            <p>
+              Ideal Canyon LTDA
+              <br />
+              CNPJ: 00.000.000/0000-00
+            </p>
+            <br />
+            <p>Rua das Flores, 123 - Centro, São Paulo/SP</p>
+            <br />
+            <p>
+              Telefone: (11) 5555-1234
+              <br />
+              E-mail: ideal.canyon@contato.com
+            </p>
+          </div>
+          <div className="footer-social-media">
+            <h3 className="text-main text-center text-demi">Redes sociais</h3>
+            <p>
+              Instagram:
+              <br />
+              <a href="">instagram.com/ideal_canyon</a>
+            </p>
+            <br />
+            <p>
+              Facebook:
+              <br />
+              <a href="">facebook.com/of_ideal_canyon</a>
+            </p>
+            <br />
+            <p>
+              Twitter:
+              <br />
+              <a href="">twitter.com/ideal_canyon1</a>
+            </p>
+          </div>
+          <div className="footer-legislatives">
+            <h3 className="text-main text-center text-demi">Legislativos</h3>
+            <a href="">Política de privacidade</a>
+            <br />
+            <br />
+            <a href="">Termo de uso</a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
