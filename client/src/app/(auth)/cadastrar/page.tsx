e612efeb400f4c9da7885e2698a9ab0f28d1023a
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-29 13:53:39 -0300

    fix: removed Layout component

--- client/src/app/(auth)/cadastrar/page.tsx
+++ client/src/app/(auth)/cadastrar/page.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent } from "react";
 
-import Layout from "../../../components/Layout/Layout";
 import Register from "../../../components/Auth/Register";
 import Navbar from "../../../components/Navbar/Navbar";
 import Footer from "../../../components/Footer/Footer";
@@ -8,15 +7,13 @@ import Footer from "../../../components/Footer/Footer";
 type CadastrarProps = {};
 
 const Cadastrar: FunctionComponent<CadastrarProps> = () => {
-    return (
-        <>
-            <Layout>
-                <Navbar />
-                <Register />
-                <Footer />
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Register />
+      <Footer />
+    </>
+  );
 };
 
 export default Cadastrar;
