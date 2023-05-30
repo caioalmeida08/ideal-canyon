70c9fb4a4f08288c5e4b7756ec07a0cf08b59bec
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-29 21:50:22 -0300

    fix: moved query provider back to home page, due to issues with use client directive

--- client/src/app/(Home)/page.tsx
+++ client/src/app/(Home)/page.tsx
@@ -1,30 +1,36 @@
 "use client";
 
-import Navbar from "@/components/Navbar/Navbar";
-import NavLink from "@/components/Navbar/NavLink";
-import Hero from "@/components/Home/Hero";
-import Review from "@/components/Home/Review";
-import Benefits from "@/components/Home/Benefits";
-import Product from "@/components/Home/Product";
-import MeetTheTeam from "@/components/Home/MeetTheTeam";
-import FAQ from "@/components/Home/Faq";
-import DoubtsForm from "@/components/Home/DoubtsForm";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Navbar from "../../components/Navbar/Navbar";
+import NavLink from "../../components/Navbar/NavLink";
+import Hero from "../../components/Home/Hero";
+import Review from "../../components/Home/Review";
+import Benefits from "../../components/Home/Benefits";
+import Product from "../../components/Home/Product";
+import MeetTheTeam from "../../components/Home/MeetTheTeam";
+import FAQ from "../../components/Home/Faq";
+import DoubtsForm from "../../components/Home/DoubtsForm";
+
+const queryClient = new QueryClient();
 
 export default function Home() {
   return (
     <>
-      <Navbar>
-        <NavLink href="#products_section">Modelos</NavLink>
-        <NavLink href="#doubts_form">Contato</NavLink>
-        <NavLink href="#footer">Sobre</NavLink>
-      </Navbar>
-      <Hero />
-      <Review />
-      <Benefits />
-      <Product modelShortProp="canyon-elite-scooter" />
-      <MeetTheTeam />
-      <FAQ />
-      <DoubtsForm />
+      <QueryClientProvider client={queryClient}>
+        <Navbar>
+          <NavLink href="#products_section">Modelos</NavLink>
+          <NavLink href="#doubts_form">Contato</NavLink>
+          <NavLink href="#footer">Sobre</NavLink>
+        </Navbar>
+        <Hero />
+        <Review />
+        <Benefits />
+        <Product modelShortProp="canyon-elite-scooter" />
+        <MeetTheTeam />
+        <FAQ />
+        <DoubtsForm />
+      </QueryClientProvider>
     </>
   );
 }
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import Footer from "../components/Footer/Footer";
 import Layout from "../components/Layout/Layout";
 
@@ -7,8 +5,6 @@ export const metadata = {
   title: "Ideal Canyon",
   description: "O lugar ideal para encontrar a sua scooter.",
 };
-
-const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -18,12 +14,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body>
-        <QueryClientProvider client={queryClient}>
           <Layout>
             {children}
             <Footer />
           </Layout>
-        </QueryClientProvider>
       </body>
     </html>
   );
