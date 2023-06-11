36380bea449f48ea4ae2948ce9d79b9a1c055285
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-06-11 11:45:20 -0300

    fix: fixed fullname input pattern

--- client/src/components/Home/DoubtsForm.tsx
+++ client/src/components/Home/DoubtsForm.tsx
@@ -28,12 +28,12 @@ const DoubtsForm = () => {
             "Content-Type": "application/json",
           },
           validateStatus(status) {
-              return status == 201
-          },
+            return status == 201;
+          }
         });
-        return data
+        return data;
       } catch (error: any) {
-        handleReactQueryError(error)
+        throw handleReactQueryError(error);
       }
     },
     enabled: isQueryEnabled,
@@ -68,18 +68,14 @@ const DoubtsForm = () => {
           <br />
           Entre em contato
         </h2>
-        <form
-          method="POST"
-          onSubmit={handleSubmit}
-          id="doubts_form"
-        >
+        <form method="POST" onSubmit={handleSubmit} id="doubts_form">
           <InputText
             label="Nome completo"
             name="contact_name"
             placeholder="Digite seu nome completo"
             required
-            pattern="[a-zA-Z\s]+"
-            title="O nome deve conter apenas letras maiúsculas e minúsculas"
+            pattern="^([a-zA-ZÀ-ú]+[ ]{1}[a-zA-ZÀ-ú]+)+$"
+            title="O nome deve conter apenas letras maiúsculas e minúsculas, além de espaço. No mínimo 1 sobrenome."
           />
           <InputText
             label="Assunto"
@@ -113,10 +109,18 @@ const DoubtsForm = () => {
           <ButtonSubmit text="Enviar mensagem" />
           {isQueryEnabled && (
             <span id="error_display" className={style.error_display}>
-            {(isLoading && isQueryEnabled) && <CircularProgress/>}
-            {isError && <span className="text-red-800">{JSON.stringify(error)}</span>}
-            {!(isLoading || isError) && <span className="text-green-600 font-semibold line-height-15">{data.data.message}</span>}
-          </span>
+              <>
+                {isLoading && <CircularProgress />}
+                {isError && (
+                  <span className="text-red-800">{JSON.stringify(error)}</span>
+                )}
+                {!(isLoading || isError) && (
+                  <span className="text-green-600 font-semibold line-height-15">
+                    {data.data.message}
+                  </span>
+                )}
+              </>
+            </span>
           )}
         </form>
       </section>
--- client/src/lib/errors/handleReactQueryError.tsx
+++ client/src/lib/errors/handleReactQueryError.tsx
@@ -1,10 +1,8 @@
 export default function handleReactQueryError(error: any){
-    let responseMessage = error.response.data.message as string
-
-    // if its an axios error
-    if (error.name == "AxiosError" || !responseMessage){
+    // if its an error 500 (server down)
+    if (error.status == 500){
         throw "Ocorreu um erro no nosso servidor. Por favor, tente novamente mais tarde."
     }
 
-    return responseMessage
+    return error.response.data.message[0]
 }
